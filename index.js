fde3201a41f19c9ba7cedd3c5bedb3d3f654a4c2
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-03-08 23:11:19 -0500

    sword wasn't working due to style missing

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function newImage (src, position, left, right) {
     newImage.style.bottom = right
     document.body.append(newImage)
 }
+
+newImage('assets/pine-tree.png','fixed', '450px', '200px') 
+newImage('assets/tree.png','fixed', '200px', '300px') 
+newImage('assets/pillar.png','fixed', '350px', '100px') 
+newImage('assets/crate.png','fixed', '150px', '200px') 
+newImage('assets/well.png ','fixed', '500px', '425px')
+newImage('assets/green-character.gif','fixed', '100px', '100px')
+
 // let newImage = document.createElement('img')
 // newImage.src = src 
 // newImage.style.position = 'fixed'
@@ -20,9 +28,16 @@ function newImage (src, position, left, right) {
 // pineTree.style.bottom = '200px'
 // document.body.append(pineTree)
 
-newImage('assets/pine-tree.png','fixed', '450px', '200px') 
-newImage('assets/tree.png','fixed', '200px', '300px') 
-newImage('assets/pillar.png','fixed', '350px', '100px') 
-newImage('assets/crate.png','fixed', '150px', '200px') 
-newImage('assets/well.png ','fixed', '500px', '425px')
-newImage('assets/green-character.gif','fixed', '100px', '100px')
+function newItem () {
+    let sword = document.createElement('img')
+        sword.src = 'assets/sword.png'
+        sword.style.position = 'fixed'
+        sword.style.left = '500px'
+        sword.style.bottom = '405px'
+        document.body.append(sword)
+        sword.addEventListener('click', function(){
+        sword.remove()
+    })
+}
+
+newItem()
