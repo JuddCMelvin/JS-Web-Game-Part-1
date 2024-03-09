bec476eae1a4fb5feae950dab7e843b4cdeed353
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-03-08 21:12:17 -0500

    created function

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (src, position, left, right) {
+    let newImage = document.createElement('img')
+    newImage.src = src 
+    newImage.style.position = position
+    newImage.style.left = left
+    newImage.style.bottom = right
+    document.body.append(newImage)
+}
+// let newImage = document.createElement('img')
+// newImage.src = src 
+// newImage.style.position = 'fixed'
+// newImage.style.left = '100px'
+// newImage.style.bottom = '100px'
+// document.body.append(newImage)
 
-let pineTree = document.createElement('img')
-greenCharacter.src = 'assets/pine-tree.png'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '450px'
-greenCharacter.style.bottom = '200px'
-document.body.append(greenCharacter)
+// let pineTree = document.createElement('img')
+// pineTree.src = 'assets/pine-tree.png'
+// pineTree.style.position = 'fixed'
+// pineTree.style.left = '450px'
+// pineTree.style.bottom = '200px'
+// document.body.append(pineTree)
+
+newImage('assets/pine-tree.png','fixed', '300px', '300px') 
